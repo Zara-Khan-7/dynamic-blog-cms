58a49c38f598621276fa2c1b628767a26ae10d73
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2025-01-06 02:54:55 +0500

    update

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-import  { Belleza }  from "next/font/google";
+import { Belleza } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 const belleza = Belleza({
   subsets: ["latin"],
   weight: ["400"],
 });
 
-
 export const metadata: Metadata = {
   title: "ZK Blogs",
-  description: "My Dynamic Blog Web uing CMS",
+  description: "My Dynamic Blog Web using CMS",
 };
 
 export default function RootLayout({
@@ -22,11 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body 
-      className={belleza.className}
-      >
+      <Head>
+        {/* Add the viewport meta tag for responsive design */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>ZK Blogs</title> {/* You can set the title here or in metadata */}
+      </Head>
+      <body className={belleza.className}>
         <div className="fixed z-20 w-full top-0">
-         <Navbar />
+          <Navbar />
         </div>
         {children}
         <Footer />
